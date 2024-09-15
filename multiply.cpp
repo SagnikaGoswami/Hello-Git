d4460e9f0deb024e258a5222a101ad6f36a79823
Sagnika Goswami <sagnikagoswami493@gmail.com>
2024-09-15 12:11:40 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int mul(int a, int b){
+    return a * b;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+
+    int ans = mul(a,b);
+
+    cout << ans << endl;
+    
+    return 0;
+}
